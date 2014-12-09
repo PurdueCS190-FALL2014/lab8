0e585903acadb14caff5368cf012cf04cf6d7e9d
Kirby Kohlmorgen <kirby@purdue.edu>
2014-12-08 22:43:22 -0500

    Add init

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cleanup
+rm -rf ~/cs190lab8
+
+mkdir ~/cs190lab8
+cd ~/cs190lab8
+
+wget https://raw.githubusercontent.com/liaohuqiu/android-Ultra-Pull-To-Refresh/master/ptr-lib/src/in/srain/cube/views/ptr/PtrFrameLayout.java
+wget https://raw.githubusercontent.com/liaohuqiu/android-Ultra-Pull-To-Refresh/master/ptr-lib/src/in/srain/cube/views/ptr/PtrClassicDefaultHeader.java
+wget https://raw.githubusercontent.com/rails/rails/master/activerecord/lib/active_record/attribute.rb
+wget https://raw.githubusercontent.com/rails/rails/master/activerecord/lib/active_record/core.rb
+
+
+echo -e '\ndirectory ~/cs190lab8 reset!'
